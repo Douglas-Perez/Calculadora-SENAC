78948290824f1003e7fd08e93a14fc51c7e6c63a
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-02-27 20:29:14 -0300

    Estrutura da calculadora

--- Calculadora SENAC/Program.cs
+++ Calculadora SENAC/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Calculadora_SENAC
+﻿namespace Calculadora_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int operador = 0;
+
+            Console.WriteLine("Calculadora C#");
+
+            while (operador > 4 || operador < 1)
+            {
+                Console.WriteLine("Selecione a operação que deseja executar:");
+                Console.Write("1 -> Soma\n2 -> Subtração\n3 -> Multiplicação\n4 -> Divisão\n");
+                operador = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (operador > 4 || operador < 1)
+                {
+                    Console.WriteLine("Valor inválido");
+                }
+            }
+
+            Console.WriteLine("Digite o primeiro valor");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            switch (operador)
+            {
+
+            }
         }
     }
 }
