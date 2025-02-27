fa497db28631ebe18ef826590fd1e8c371e57fa4
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-02-27 20:22:40 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora SENAC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora_SENAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
